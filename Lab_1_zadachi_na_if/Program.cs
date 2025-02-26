6d7eb236b1601216e7ac825b400ebd3e50604310
anna1568 <mangle1568@gmail.com>
2025-02-26 10:18:40 +0800

    Добавьте файлы проекта.

---
+++ Lab_1_zadachi_na_if/Program.cs
@@ -0,0 +1,131 @@
+﻿//Составить программу нахождения произведения двух наименьших из трех различных чисел.
+using System;
+//то же, что и в фор и переделать тоже проверки
+
+namespace FirstApp
+{
+    public class Logic
+    {
+        public static int[] FindTwoSmallest(int a, int b, int c)
+        {
+            int[] smallest = new int[2];
+
+            if ((a < b && b < c) || (b < a && a < c))
+            {
+                smallest[0] = a;
+                smallest[1] = b;
+            }
+            else if ((a < c && c < b) || (c < a && a < b))
+            {
+                smallest[0] = a;
+                smallest[1] = c;
+            }
+            else
+            {
+                smallest[0] = b;
+                smallest[1] = c;
+            }
+
+            return smallest;
+        }
+
+        public static int CalculateProduct(int[] numbers)
+        {
+            return numbers[0] * numbers[1];
+        }
+
+        public static bool IsDuplicate(int value, params int[] existingValues)
+        {
+            foreach (var existing in existingValues)
+            {
+                if (value == existing)
+                    return true;
+            }
+            return false;
+        }
+    }
+    
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите первое число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int b;
+            do
+            {
+                Console.Write("Введите второе число: ");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                if (Logic.IsDuplicate(b, a))
+                    Console.WriteLine("Вы ввели повторяющиеся значения, пожалуйста, повторите попытку: ");
+
+            } while (Logic.IsDuplicate(b, a));
+
+            int c;
+            do
+            {
+                Console.Write("Введите третье число: ");
+                c = Convert.ToInt32(Console.ReadLine());
+
+                if (Logic.IsDuplicate(c, a, b))
+                    Console.WriteLine("Вы ввели повторяющиеся значения, пожалуйста, повторите попытку: ");
+
+            } while (Logic.IsDuplicate(c, a, b));
+
+            int[] smallestNumbers = Logic.FindTwoSmallest(a, b, c);
+            int result = Logic.CalculateProduct(smallestNumbers);
+
+            Console.WriteLine("Ваш набор чисел: " + a + ", " + b + ", " + c);
+            Console.WriteLine("Наименьшие числа: " + smallestNumbers[0] + ", " + smallestNumbers[1]);
+            Console.WriteLine("Произведение двух чисел: " + smallestNumbers[0] + " * " + smallestNumbers[1] + " = " + result);
+        }
+    }
+}
+
+
+//Console.Write("Введите первое число: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//int b;
+//int c;
+//int min1;
+//int min2;
+//int result;
+
+//do
+//{
+//    Console.Write("Введите второе число: ");
+//    b = Convert.ToInt32(Console.ReadLine());
+//    if (a == b)
+//        Console.WriteLine("Вы ввели повторяющиеся значения, пожалуйста, повторите попытку: ");
+//} while (a == b);
+//do
+//{
+//    Console.Write("Введите третье число: ");
+//    c = Convert.ToInt32(Console.ReadLine());
+//    if (a == c || b == c)
+//        Console.WriteLine("Вы ввели повторяющиеся значения, пожалуйста, повторите попытку: ");
+//} while (a == c || b == c);
+
+//if ((a < b && b < c) || (b < a && a < c))
+//{
+//    min1 = a;
+//    min2 = b;
+//    result = a * b;
+//}
+//else if ((a < c && c < b) || (c < a && a < b))
+//{
+//    min1 = a;
+//    min2 = c;
+//    result = a * c;
+//}
+//else
+//{
+//    min1 = b;
+//    min2 = c;
+//    result = b * c;
+//}
+//Console.WriteLine("Ваш набор чисел: " + a + ", " + b + ", " + c);
+//Console.WriteLine("Наименьшие числа: " + min1 + ", " + min2);
+//Console.WriteLine("Произведение двух чисел: " + min1 + " * " + min2 + " = " + result);
---
+++ Lab_1_zadachi_na_ifTests/LogicTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApp;
+
+namespace FirstApp.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void FindTwoSmallestTest()
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            var result = Logic.FindTwoSmallest(a, b, c);
+            Assert.AreEqual(1, result[0]);
+            Assert.AreEqual(2, result[1]);
+        }
+
+        [TestMethod()]
+        public void FindTwoSmallestTest1()
+        {
+            var a = 1;
+            var b = 3;
+            var c = 2;
+
+            var result = Logic.FindTwoSmallest(a, b, c);
+            Assert.AreEqual(1, result[0]);
+            Assert.AreEqual(2, result[1]);
+        }
+
+        [TestMethod()]
+        public void FindTwoSmallestTest2()
+        {
+
+            var a = 2;
+            var b = 1;
+            var c = 3;
+
+            var result = Logic.FindTwoSmallest(a, b, c);
+            Assert.AreEqual(2, result[0]);
+            Assert.AreEqual(1, result[1]);
+        }
+
+        [TestMethod()]
+        public void FindTwoSmallestTest3()
+        {
+            var a = 2;
+            var b = 3;
+            var c = 1;
+
+            var result = Logic.FindTwoSmallest(a, b, c);
+            Assert.AreEqual(2, result[0]);
+            Assert.AreEqual(1, result[1]);
+        }
+
+        [TestMethod()]
+        public void FindTwoSmallestTest4()
+        {
+            var a = 3;
+            var b = 1;
+            var c = 2;
+
+            var result = Logic.FindTwoSmallest(a, b, c);
+            Assert.AreEqual(1, result[0]);
+            Assert.AreEqual(2, result[1]);
+        }
+
+        [TestMethod()]
+        public void FindTwoSmallestTest5()
+        {
+            var a = 3;
+            var b = 2;
+            var c = 1;
+
+            var result = Logic.FindTwoSmallest(a, b, c);
+            Assert.AreEqual(2, result[0]);
+            Assert.AreEqual(1, result[1]);
+        }
+
+        [TestMethod()]
+        public void CalculateProductTest6()
+        {
+            var numbers = new int[] { 1, 2 };
+
+            var result = Logic.CalculateProduct(numbers);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod()]
+        public void IsDuplicateTest()
+        {
+            int number = 1;
+            int[] numbers = new int[] { 1 };
+            bool result = Logic.IsDuplicate(number, numbers);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod()]
+        public void IsNotDuplicateTest()
+        {
+            int number = 2;
+            int[] numbers = new int[] { 1 };
+            bool result = Logic.IsDuplicate(number, numbers);
+            Assert.AreEqual(false, result);
+        }
+    }
+}
